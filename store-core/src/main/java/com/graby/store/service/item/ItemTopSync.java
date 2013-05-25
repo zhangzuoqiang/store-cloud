976be9f8cad66c92f71891ab6cf1b62f8bef1024
discolt <discolt@gmail.com>
2013-05-25 19:27:16 +0800

    ccc
    ccc

--- store-core/src/main/java/com/graby/store/service/item/ItemTopSync.java
+++ store-core/src/main/java/com/graby/store/service/item/ItemTopSync.java
@@ -50,7 +50,7 @@ public class ItemTopSync {
 		Long relatedId = itemService.getRelatedItemId(item.getNumIid(), sku == null ? 0L : sku.getSkuId());
 		if (relatedId != null) {
 			// 更新标题和SKU
-			//itemService.updateItemTitle(relatedId, item.getTitle(), sku == null ? "" : SkuUtils.convert(sku.getPropertiesName()));
+			itemService.updateItemTitle(relatedId, item.getTitle(), sku == null ? "" : SkuUtils.convert(sku.getPropertiesName()));
 			return;
 		}
 		com.graby.store.entity.Item copy = new com.graby.store.entity.Item();
